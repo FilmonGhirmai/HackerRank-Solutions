abeaf86263379ad9292c478ed3e2d1205a09c101
gfilmon <gfilmongp@gmail.com>
2017-09-07 07:36:38 +0300

    first commit

---
+++ src/Java/dataStructure/JavaDeque.java
@@ -0,0 +1,41 @@
+package Java.dataStructure;
+
+/**
+ * Created by
+ * Filmon Ghirmai (skynet) on 9/6/17.
+ */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+/** Program : Java Deque*/
+/**
+  Sample input
+  6 3
+  5 3 5 2 3 2
+
+  Sample output
+  3
+* */
+
+public class JavaDeque {
+    public static void display(){
+
+
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque<>();
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+            deque.add(num);
+        }
+
+
+
+    }
+}
