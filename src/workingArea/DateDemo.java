91ffdf1a38807c6298d8abe6fd26c16cde7304f7
Filmon Ghirmai <gfilmongp@gmail.com>
2017-10-11 22:10:56 +0300

    Removing /out src/workingArea ...

--- src/workingArea/DateDemo.java
+++
@@ -1,7 +0,0 @@
-package workingArea;
-
-/**
- * Created by skynet on 8/1/17.
- */
-public class DateDemo {
-}
--- src/workingArea/tes.java
+++
@@ -1,8 +0,0 @@
-package workingArea;
-
-/**
- * Created by
- * Filmon Ghirmai (skynet) on 9/5/17.
- */
-public class tes {
-}
